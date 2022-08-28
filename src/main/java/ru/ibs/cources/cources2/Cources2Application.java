9d2c3964841ba06c7d18816a267ae6347dc0829f
VBychenkova <timka_@bk.ru>
2022-08-28 21:19:12 +0300

    Commit 0

---
+++ src/main/java/ru/ibs/cources/cources2/Cources2Application.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.cources2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cources2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cources2Application.class, args);
+    }
+
+}
